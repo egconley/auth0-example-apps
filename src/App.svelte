bd355ae504a62b089dbb5a0c622bcc214cebc18b
Art Rosnovsky <art.rosnovsky@auth0.com>
2020-03-30 22:23:51 -0700

    added front page + screenshots ⏱️

---
+++ src/App.svelte
@@ -0,0 +1,73 @@
+<script>
+	import Sample from './sample.svelte'
+	import apps from '../apps.js'
+</script>
+
+<main>
+	<div class="header-brand">
+		<img src="https://cdn.auth0.com/website/press/resources/auth0-logo-monotone-black.svg" class="header-brand-img" alt="Auth0 logo">
+		<div style="margin-left: 20px;"><a href="https://dse.rosnovsky.us">DSE Sample Apps</a></div>
+	</div>
+	<div class="apps">
+		{#each apps as app}
+			<Sample app={app} />
+		{/each}
+	</div>
+</main>
+
+<style>
+	main {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		padding: 0;
+		max-width: 100vw;
+		margin: 0 auto;
+	}
+
+	.header-brand {
+		display: flex;
+		width: 100vw;
+		height: 7rem;
+		align-items: center;
+		justify-content: center;
+		margin: 0;
+		font-weight: 400;
+		line-height: 1.5;
+		color: #495057;
+		text-align: center;
+	}
+
+	.header-brand a {
+		color: inherit;
+		margin-right: 1rem;
+		font-size: 1.25rem;
+		white-space: nowrap;
+		font-weight: 600;
+		padding: 0;
+		transition: opacity .3s;
+		line-height: 2rem;
+		text-decoration: none;
+	}
+
+	.header-brand-img {
+		height: 2rem;
+		line-height: 2rem;
+		vertical-align: bottom;
+		margin-right: .5rem;
+		width: auto;
+	}
+
+	.apps {
+		display: flex;
+		width: 80%;
+		justify-content: space-around;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/sample.svelte
@@ -0,0 +1,61 @@
+<script>
+    export let app;
+    const {name, stack, description, deploy, screenshot, url, quickstart} = app;
+</script>
+
+<style>
+main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 25rem;
+    /* border: 1px solid lightgray; */
+    border-radius: 0.25em;
+    /* background-image: linear-gradient(-30deg, #e9e8e3, #ebeae5); */
+    box-shadow: 2px 7px 10px 4px rgba(235, 235, 235, 0.4),
+            inset 1px 1px 1px 1px rgba(216, 216, 216, 0.2),
+            inset -1px -1px 1px 1px rgba(231, 220, 220, 0.3);
+}
+
+.screenshot {
+    width: 300px;
+    height: 200px;
+    align-self: center;
+    border: 1px solid lightblue
+}
+
+.description {
+    width: 90%;
+}
+
+.docs {
+    width: 100%;
+    display: flex;
+    padding: 0;
+    list-style: none;
+    justify-content: space-around;
+    align-items: center;
+    color: darkgray;
+}
+
+.docs a {
+    color: inherit;
+}
+
+.deploy {
+    margin: 1rem;
+}
+</style>
+
+<main>
+    <h2>{name}</h2>
+    <img src={screenshot} alt={name} class="screenshot" />
+    <p><a href={url} target="_blank" norefere noopener>{url}</a></p>
+    <p class="description">{description}</p>
+    <div class="docs">
+        <p><a href={quickstart}>{stack.name} Quickstart</a></p>
+        <p><a href={stack.url}>{stack.name} Docs</a></p>
+    </div>
+    <a href={deploy} class="deploy"><img src="https://zeit.co/button" alt="Deploy to ZEIT Now" /></a>
+</main>
