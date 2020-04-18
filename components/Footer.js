63eb0bb7b6fd9d37700e664a4f0b3276ac77b876
Art Rosnovsky <art.rosnovsky@auth0.com>
2020-04-18 14:01:48 -0700

    Fixed footer link colors

--- components/Footer.js
+++ components/Footer.js
@@ -5,14 +5,14 @@ const Footer = () => {
         <Container className="pt-4 my-md-5 pt-md-5 border-top">
             <Row className="container container-fluid mx-auto">
                 <Col className="col-12 col-md">
-                    <a className="mx-auto" href="https://auth0.com"><img src="/auth0-logo-forlightbg.png" width="50px" alt="Auth0 Logo" /></a>
+                    <a className="mx-auto mb-1" href="https://auth0.com"><img src="/auth0-logo-forlightbg.png" width="50px" alt="Auth0 Logo" /></a>
                     <p>© 2020 Auth0, Inc </p>
                 </Col>
                 <Col className="col-6 col-md d-block">
                     <h5>Tools</h5>
                     <ul className="list-unstyled text-small">
                         <li>
-                            <a href="https://manage.auth0.com/#/applications" className="text-muted">Manage Dashboard</a>
+                            <a href="https://manage.auth0.com/#/applications" className="text-dark">Manage Dashboard</a>
                         </li>
                         <li>
                             <a href="https://github.com/rosnovsky/auth0-example-apps/blob/master/auth0/tenant.yaml" className="text-muted">Download tenant settings</a>
@@ -21,15 +21,34 @@ const Footer = () => {
                             <a href="https://github.com/rosnovsky/auth0-example-apps" className="text-muted">Fork on Github</a>
                         </li>
                         <li>
-                            <a href="https://har.auth0.net" className="text-dark">HAR Inspector</a>
+                            <a href="https://har.auth0.net" className="text-muted">HAR Inspector</a>
                         </li>
                     </ul>
                 </Col>
                 <Col className="col-6 col-md">
                     <h5>Auth0 Resources</h5>
+                    <ul className="list-unstyled text-small">
+                        <li>
+                            <a href="https://auth0.com/docs/videos/learn-identity" className="text-dark">Learn Identity</a>
+                        </li>
+                        <li>
+                            <a href="https://auth0.com/docs/videos/get-started/" className="text-muted">Getting Started</a>
+                        </li>
+                        <li>
+                            <a href="https://support.auth0.com/" className="text-muted">Help & Support</a>
+                        </li>
+                        <li>
+                            <a href="https://auth0.com/blog/" className="text-muted">Auth0 Blog</a>
+                        </li>
+                    </ul>
                 </Col>
                 <Col className="col-6 col-md">
                     <h5>Authentication</h5>
+                    <ul className="list-unstyled text-small">
+                        <li>
+                            <a href="https://oauth.net/2/" className="text-dark">OAuth 2.0</a>
+                        </li>
+                    </ul>
                 </Col>
             </Row>
         </Container>
