a48165ff547deba5388a45ebeec96bfc261e2929
Art Rosnovsky <art.rosnovsky@auth0.com>
2020-04-26 20:24:52 -0700

    debugging build some more

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -27,7 +27,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} user={getProfile()}/>
+      <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
           margin: `0 auto`,
