88803aa0c3339f2929bbc12e98287cf1684945db
Art Rosnovsky <art.rosnovsky@auth0.com>
2020-05-02 15:31:11 -0700

    Living dangarously: fixed app page 🧨

--- web/src/pages/app.js
+++ web/src/pages/app.js
@@ -11,12 +11,14 @@ import {
 } from "reactstrap"
 import React, { useState, useEffect} from "react"
 import Layout from "../components/layout"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import SEO from "../components/seo"
 import { ProfilePlaceholder } from "../pages/profile"
 
 const AppPage = ({ data }) => {
     const [siteScreenshot, setSiteScreenshot] = useState();
+    const app = data.sanityApp
+    const slug = `app/${app.slug.current}`
 
     useEffect(() => {
         // This is a temporary proof-of concept.
@@ -25,17 +27,17 @@ const AppPage = ({ data }) => {
     
         // For now, this code just fetches a single screenshot which acts as a placeholder.
         const screenshot = async () => {
-          return fetch("https://dse.netlify.app/.netlify/functions/screenshot", { headers: { "Accept": "application/json" } })
-              .then(response => response.json())
-              .then(data => {
-                  setSiteScreenshot(data);
-                  })
-          .catch(error => ({ statusCode: 422, body: String(error) }));
-      };
-      screenshot();
-      }, [])
+            return fetch("https://dse.netlify.app/.netlify/functions/screenshot", { headers: { "Accept": "application/json" } })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    setSiteScreenshot(data);
+                    })
+            .catch(error => ({ statusCode: 422, body: String(error) }));
+        };
+        screenshot();
+        }, [])
 
-    const app = data.sanityApp
     return (
         <Layout>
             <SEO title={app.title} />
@@ -43,47 +45,46 @@ const AppPage = ({ data }) => {
                 <Row className="pb-5">
                     <Col className="d-flex mx-auto justify-content-center">
                         <Card style={{ width: 24 + "rem" }} className="mx-auto mt-5">
+                            <Link to={slug}>
                             {siteScreenshot ?
                                 <CardImg
                                 top
                                 style={{ minHeight: "200px" }}
                                 src={siteScreenshot}
-                                alt={app.node.title}
+                                alt={app.title}
                                 loading="lazy"
                                 />
                             : <ProfilePlaceholder type="screenshot" /> }
+                            </Link>
                             <CardBody>
-                                <CardTitle>
-                                    <h2>{app.title}</h2>
-                                </CardTitle>
-                                <CardText>{app._rawDescription[0].children[0].text}</CardText>
-                                <Row className="mx-auto justify-content-between">
-                                    <p>
-                                    <a href={app.quickstart} className="text-muted">
-                                        Quickstart
-                                    </a>
-                                    </p>
-                                    <p>
-                                    <a href={app.docs} className="text-muted">
-                                        Docs
-                                    </a>
-                                    </p>
-                                </Row>
-                                <Row className="mx-auto justify-content-between">
-                                    <p className="text-muted">
-                                        Tech Stack: <img src={app.technology.logo.asset.url} width="20px" alt={app.technology.title} />
-                                    </p>
-                                </Row>
-                                <Row className="mx-auto justify-content-center mb-3">
-                                    <a href={app.deploy} className="deploy">
-                                    <img src="https://zeit.co/button" alt="Deploy to ZEIT Now" />
-                                    </a>
-                                </Row>
-                                <Row className="mx-auto justify-content-center mb-3">
-                                    <Button className="btn-block btn-light" href={app.url}>
-                                    {app.title} &rarr;
-                                    </Button>
-                                </Row>
+                            <CardTitle>
+                                <Link to={slug}>
+                                <h2>{app.title}</h2>
+                                </Link>
+                            </CardTitle>
+                            <CardText>{app._rawDescription[0].children[0].text}</CardText>
+                            <Row className="mx-auto justify-content-between">
+                                <p>
+                                <a href={app.quickstart} className="text-muted">
+                                    Quickstart
+                                </a>
+                                </p>
+                                <p>
+                                <a href={app.docs} className="text-muted">
+                                    Docs
+                                </a>
+                                </p>
+                            </Row>
+                            <Row className="mx-auto justify-content-center mb-3">
+                                <a href={app.deploy} className="deploy">
+                                <img src="https://www.netlify.com/img/deploy/button.svg" alt="Deploy to Netlify" />
+                                </a>
+                            </Row>
+                            <Row className="mx-auto justify-content-center mb-3">
+                                <Button className="btn-block btn-light" href={app.url}>
+                                {app.title} &rarr;
+                                </Button>
+                            </Row>
                             </CardBody>
                         </Card>
                     </Col>
